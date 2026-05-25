696596f17ebf73ebce066db5f658e15763aef1c3
edwinHernandez <edwinhb139@gmail.com>
2026-05-25 15:23:09 +0200

    initial commit, components and router

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Curriculum } from "./components/Curriculum";
+import {RouterNavigation} from "./router/RouterNavigation";
+
+function App() {
+  return (
+    <>
+      <RouterNavigation />
+    </>
+  );
+}
+
+export default App;
---
+++ src/router/RouterNavigation.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
+import { Curriculum } from "../components/Curriculum";
+import { Index } from "../components/Index";
+import { Portfolio } from "../components/Portfolio";
+import { Contact } from "../components/Contact";
+import { Services } from "../components/Services";
+
+export const RouterNavigation = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/index" element={<Index />} />
+                <Route path="/Portfolio" element={<Portfolio />} />
+                <Route path="/curriculum" element={<Curriculum />} />
+                <Route path="/Contact" element={<Contact />} />
+                <Route path="/Services" element={<Services />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
